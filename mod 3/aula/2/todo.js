4cd827b194231b6c36037525c38d96d25a8d5626
unknown <matheus_c_neves@saojose.sc.senai.br>
2019-06-17 12:22:40 -0300

    Var todos no todo.js

--- mod 3/aula/2/todo.js
+++ mod 3/aula/2/todo.js
@@ -1,1 +1,12 @@
-alert ("está funcionando...")
+var ListElement =
+document.querySelector("#app ul");
+var InputElement =
+document.querySelector("#app input");
+var ButtonElement = 
+document.querySelector("#app button");
+
+var todos = [
+    'fazer café',
+    'estudar js',
+    'qualquer coisa',
+];
