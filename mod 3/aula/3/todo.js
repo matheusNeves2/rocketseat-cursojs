e8cc719d99a1abd5f5572d9cb689ad90dda427b7
unknown <matheus_c_neves@saojose.sc.senai.br>
2019-06-17 12:26:53 -0300

    Inicio modulo 3 aula 3

---
+++ mod 3/aula/3/todo.js
@@ -0,0 +1,12 @@
+var ListElement =
+document.querySelector("#app ul");
+var InputElement =
+document.querySelector("#app input");
+var ButtonElement = 
+document.querySelector("#app button");
+
+var todos = [
+    'fazer café',
+    'estudar js',
+    'qualquer coisa',
+];
